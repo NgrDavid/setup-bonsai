30319d94e1382632a267953f74b6aab0091ad00c
David Maas <d.maas+git@neurogears.org>
2025-05-08 19:55:08 -0500

    Test

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import * as glob from '@actions/glob';
 
 async function main() {
     console.log("Found environments:");
-    const environmentPaths = await glob.create(core.getInput('environment-path', { required: true }));
+    const environmentPaths = await glob.create(core.getInput('environment-paths', { required: true }));
     for await (const environmentPath of environmentPaths.globGenerator()) {
         console.log(`'${environmentPath}'`);
     }
